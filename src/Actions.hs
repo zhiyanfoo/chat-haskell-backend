a37f00e0e283e100034d0f4b5a12e62aeb7f2247
Zhiyan Foo <zhiyanfoo@gmail.com>
2019-04-13 20:24:31 -0400

    Parsing works!!

--- src/Actions.hs
+++ src/Actions.hs
@@ -4,18 +4,42 @@
 
 module Main where
 
-import Data.Aeson (Value(..), (.=), encode, object, decode)
-import GHC.Exts (fromList)
+import Data.Aeson
+  ( FromJSON
+  , Value(..)
+  , (.:)
+  , (.=)
+  , decode
+  , encode
+  , object
+  , parseJSON
+  , withArray
+  , withObject
+  )
+import Data.Aeson.Types (Parser, parseMaybe)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
-import Data.Aeson.Types (Parser, parseMaybe)
+import Data.Text (Text)
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as BC
 import qualified Data.Vector as V
+import GHC.Exts (fromList)
+
+jsonFile :: FilePath
+jsonFile = "test.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
 
 main = do
   putStrLn "ASD"
   print $ encode val
   print $ encode val2
-  print $ parseMaybe parseTuple =<< decode "{\"a\":\"he\",\"b\":true}" 
+  print $ parseMaybe parseArray2 =<< decode "[{\"a\":\"he\",\"b\":true}]"
+  file <- getJSON
+  print $ (decode file :: Maybe Action)
+  BC.putStrLn file
 
 val :: Value
 val =
@@ -29,21 +53,60 @@ val2 = object ["bol" .= True, "numb" .= ([1, 2, 3] :: [Int])]
 parseTuple :: Value -> Parser (String, Bool)
 parseTuple (Object obj) = do
   let mbFieldA = HM.lookup "a" obj
-  fieldA <- case mbFieldA of
-    Just x -> return x
-    Nothing -> fail "no field 'a'"
-
-  a <- case fieldA of
-          String x -> return (T.unpack x)
-          _ -> fail "expected a string"
-
-  b <- case HM.lookup "b" obj of
-         Just (Bool x) -> return x
-         Just _ -> fail "expected a boolean"
-         Nothing -> fail "no field 'b'"
-
+  fieldA <-
+    case mbFieldA of
+      Just x -> return x
+      Nothing -> fail "no field 'a'"
+  a <-
+    case fieldA of
+      String x -> return (T.unpack x)
+      _ -> fail "expected a string"
+  b <-
+    case HM.lookup "b" obj of
+      Just (Bool x) -> return x
+      Just _ -> fail "expected a boolean"
+      Nothing -> fail "no field 'b'"
   return (a, b)
 
 parseArray :: Value -> Parser [(String, Bool)]
 parseArray (Array arr) = mapM parseTuple (V.toList arr)
 parseArray _ = fail "expected an array"
+
+parseArray2 :: Value -> Parser [(String, Bool)]
+parseArray2 =
+  withArray "array of tuples" $ \arr -> mapM parseTuple (V.toList arr)
+
+parseTuple2 :: Value -> Parser (String, Bool)
+parseTuple2 =
+  withObject "tuple" $ \obj -> do
+    a <-
+      case HM.lookup "a" obj of
+        Just x -> parseJSON x
+        Nothing -> fail "no field a"
+    b <-
+      case HM.lookup "b" obj of
+        Just x -> parseJSON x
+        Nothing -> fail "no field b"
+    return (a, b)
+
+parseTuple3 :: Value -> Parser (String, Bool)
+parseTuple3 =
+  withObject "tuple" $ \o -> do
+    a <- o .: "a"
+    b <- o .: "b"
+    return (a, b)
+
+data Action
+  = AddMessage { author :: Text
+               , message :: Text }
+  | AddUser { username :: Text }
+  deriving (Show)
+
+instance FromJSON Action where
+  parseJSON =
+    withObject "action" $ \o -> do
+      kind <- o .: "type"
+      case kind of
+        "ADD_MESSAGE" -> AddMessage <$> o .: "author" <*> o .: "message"
+        "ADD_USER" -> AddUser <$> o .: "username"
+        _ -> fail ("unknown kind: " ++ kind)
